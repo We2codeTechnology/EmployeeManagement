505741f2a28deb6404a35e548933cf672416bd1e
shivaniwe2code <vijendra.patel.we2code@gmail.com>
2022-09-29 16:38:05 +0530

    backnedpush

--- New folder/client/src/dashboard.js
+++ New folder/client/src/dashboard.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React,{useEffect, useState, useMemo } from 'react';
+import Axios from 'axios';
 
 const Dashboard = () => {
+    const [employeeList, setEmployeeList] = useState([]);
+    const getEmployees = () => {
+        Axios.get("https://apnaorganicstore.in/index/employees").then((response) => {
+          setEmployeeList(response.data);
+          
+        });
+      };
+      useEffect(() => {
+        getEmployees();
+      },[]);
     return (
         <div>
-            <h1>HELLO HI HOW R U BYYE</h1>
+        {(employeeList || []).map((data) => {
+            return (
+                <p>{data.staff_name}</p>
+
+            )
+
+        })}
         </div>
     );
 }
