bd14d6606a862e884aa7b057c968a58af95c0ff9
shivaniwe2code <vijendra.patel.we2code@gmail.com>
2022-09-30 12:59:34 +0530

    Updates

---
+++ precache-manifest.30cd24954b3406195bb1fcd6434930f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47c4efec475a3260b6aa",
+    "url": "/EmployeeManagement/static/css/main.6a66613d.chunk.css"
+  },
+  {
+    "revision": "47c4efec475a3260b6aa",
+    "url": "/EmployeeManagement/static/js/main.47c4efec.chunk.js"
+  },
+  {
+    "revision": "084b5a102f666b01c2c5",
+    "url": "/EmployeeManagement/static/js/1.084b5a10.chunk.js"
+  },
+  {
+    "revision": "6f2be0dbdb989fb866f5",
+    "url": "/EmployeeManagement/static/js/2.6f2be0db.chunk.js"
+  },
+  {
+    "revision": "3f7538d29805432ade92",
+    "url": "/EmployeeManagement/static/js/runtime~main.3f7538d2.js"
+  },
+  {
+    "revision": "4fd4428d75416232da1c2c1ff7f58b7c",
+    "url": "/EmployeeManagement/index.html"
+  }
+];
